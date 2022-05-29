6ddb544e4327fcac348acd251eb3bc28b530f802
atharva-gadekar <atharvagadekar@gmail.com>
2022-05-29 19:06:19 +0530

    Stage 1 completed

---
+++ first.js
@@ -0,0 +1,16 @@
+var card = document.getElementById("card");
+
+
+card.addEventListener("click",()=>{
+    confetti.start();
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "block";
+    setTimeout(()=>{
+        document.getElementById("two").style.display = "none";
+        document.getElementById("three").style.display = "flex";
+        setTimeout(()=>{
+            window.open("index.html","_self");
+        },4000)
+    },4000)
+
+})
